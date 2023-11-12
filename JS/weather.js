898c046126f33af6b46613fcfd35fe73a894d37b
Hwakyung Alice Park <hkyungalice@gmail.com>
2023-11-13 03:21:42 +0900

    weather

---
+++ JS/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = '1ed3cc8ee930f1029a9b4cd4b7e3b199';
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector('#weather span:first-child');
+      const city = document.querySelector('#weather span:last-child');
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
